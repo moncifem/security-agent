19b5ceba1a489caa6cc9b651c93a684d0a9d941a
Moncif <moncif@moncif.com>
2025-05-29 14:53:22 +0200

    Supervisor security agent

---
+++ security_agent.py
@@ -0,0 +1,25 @@
+from langgraph_supervisor import create_supervisor
+from langgraph.prebuilt import create_react_agent
+from langchain_anthropic import ChatAnthropic
+from utils.model import model
+from agents import swagger_agent
+
+
+security_agent = create_supervisor(
+    [swagger_agent],
+    model=model,
+    prompt="You are a security expert. Always use one tool at a time."
+).compile(name="security_agent")
+
+
+if __name__ == "__main__":
+    config = {
+        "configurable": {
+            "thread_id": "1"  
+        }   
+    }
+    security_response = security_agent.invoke(
+        {"messages": [{"role": "user", "content": "get the swagger from http://localhost:8000/openapi.json"}]},
+        config
+    )
+    print(security_response)
